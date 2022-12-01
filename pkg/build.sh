836025e587ddefb757d3c780e58ab22a1c1ae60f
Christian Hammacher <bmasterc@gmail.com>
2022-12-01 20:25:46 +0100

    Use XZ instead of new ZSTD compression for debs

--- pkg/build.sh
+++ pkg/build.sh
@@ -25,7 +25,7 @@ sed -i "s/VERSION/$version/g" /tmp/maintenancetimersplugin/maintenancetimersplug
 
 echo "- Packaging files..."
 cd /tmp/maintenancetimersplugin
-dpkg-deb --build maintenancetimersplugin_$version
+dpkg-deb --build -Zxz maintenancetimersplugin_$version
 dpkg-sig -k $signkey -s builder maintenancetimersplugin_$version.deb
 mv /tmp/maintenancetimersplugin/maintenancetimersplugin_$version.deb $pkgdir/maintenancetimersplugin_$version.deb
 #rm -rf /tmp/maintenancetimersplugin/maintenancetimersplugin_$version
